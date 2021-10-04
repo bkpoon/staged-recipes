32f7f0888470ee2b7834012e00cdf472c80c6a78
Billy K. Poon <bkpoon@lbl.gov>
2021-10-03 23:11:27 -0700

    Add libcifpp

---
+++ recipes/libcifpp/bld.bat
@@ -0,0 +1,14 @@
+cmake %CMAKE_ARGS% --trace-expand -S . -B build ^
+  -DCMAKE_INSTALL_PREFIX=%PREFIX% ^
+  -DBUILD_SHARED_LIBS=ON
+
+REM cmake always creates a Debug build
+REM cmake --build build --config Release
+cd build
+msbuild libcifpp.sln /property:Configuration=Release
+cd ..
+cmake --install build
+
+ctest --output-on-failure --test-dir build -C Release
+
+dir build
---
+++ recipes/libcifpp/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -xe
+
+# instructions from https://github.com/PDB-REDO/libcifpp#building
+cmake ${CMAKE_ARGS} --trace-expand -S . -B build \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_CXX_COMPILER=${CXX} \
+  -DCMAKE_BUILD_TYPE=Release
+
+cmake --build build
+cmake --install build
+
+ctest --output-on-failure --test-dir build || true
