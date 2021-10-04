fa8622dc92ea7e705adccdf209084710779e2a42
Billy K. Poon <bkpoon@lbl.gov>
2021-10-04 10:51:57 -0700

    Set installation prefix

--- recipes/libcifpp/build.sh
+++ recipes/libcifpp/build.sh
@@ -6,9 +6,12 @@ if [[ ! -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
   CXXFLAGS=`echo $CXXFLAGS | sed 's/c++14/c++17/g'`
 fi
 
+# instructions from https://github.com/PDB-REDO/libcifpp#building
+# - use conda cmake_args
+# - enable shared libraries
 mkdir build
 cd build
-cmake -DBUILD_SHARED_LIBS=ON ..
+cmake ${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON ..
 cmake --build . --config Release
 ctest -C Release
 cmake --install .
