982b01d24064e078c7565e9aae7898b29753b71a
Billy K. Poon <bkpoon@lbl.gov>
2021-10-04 22:12:38 -0700

    Try fixing std::filesystem on macOS

--- recipes/libcifpp/build.sh
+++ recipes/libcifpp/build.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 set -xe
 
-# enable C++17 on macOS
-if [[ ! -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
-  CXXFLAGS=`echo $CXXFLAGS | sed 's/c++14/c++17/g'`
-fi
-
 # instructions from https://github.com/PDB-REDO/libcifpp#building
 # - use conda cmake_args
+# - enable C++17 on macOS
+# - link libc++fs on macOS
 # - enable shared libraries
 mkdir build
 cd build
-cmake ${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON ..
+# if [[ ! -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
+#   CXXFLAGS=`echo $CXXFLAGS | sed 's/c++14/c++17/'`
+#   cmake ${CMAKE_ARGS} -DCXX_FILESYSTEM_NO_LINK_NEEDED=FALSE -DBUILD_SHARED_LIBS=ON ..
+# else
+#   cmake ${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON ..
+# fi
+
+if [[ ! -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
+  export MACOSX_DEPLOYMENT_TARGET=10.15
+  CMAKE_ARGS=`echo $CMAKE_ARGS | sed 's/OSX_DEPLOYMENT_TARGET=10\.9/OSX_DEPLOYMENT_TARGET=10\.15/'`
+  cmake ${CMAKE_ARGS} -DCXX_FILESYSTEM_NO_LINK_NEEDED=TRUE -DBUILD_SHARED_LIBS=ON ..
+else
+  cmake ${CMAKE_ARGS} -DBUILD_SHARED_LIBS=ON ..
+fi
+
 cmake --build . --config Release
 ctest -C Release
 cmake --install .
