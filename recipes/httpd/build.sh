64bf12b9fa0bd2ffb495ac365b81c9fb092d6fcc
Billy K. Poon <bkpoon@lbl.gov>
2024-04-12 11:31:05 -0700

    Add script

--- recipes/httpd/build.sh
+++ recipes/httpd/build.sh
@@ -1,3 +1,35 @@
 #!/bin/bash
 
-ls
+CORES=4
+
+echo "Building apr"
+cd apr
+./configure --prefix=${PREFIX}
+make -j ${CORES}
+make install
+cd ..
+echo
+
+echo "Building apr-util"
+cd apr-util
+./configure --prefix=${PREFIX} --with-apr=${PREFIX} --with-expat=${PREFIX} --with-openssl=${PREFIX}
+make -j ${CORES}
+make install
+cd ..
+echo
+
+echo "Building httpd"
+cd httpd
+./configure --prefix=${PREFIX} --with-ssl=${PREFIX} --with-apr=${PREFIX} --with-apr-util=${PREFIX}
+make -j ${CORES}
+make install
+cd ..
+echo
+
+echo "Building ${WSGI}"
+cd wsgi
+./configure --prefix=${PREFIX} --with-apxs=${PREFIX}/bin/apxs --with-python=${PREFIX}/bin/python3
+make -j ${CORES}
+make install
+cd ..
+echo
