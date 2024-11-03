4967efa6db9d13ff2156216aad7c915708432650
Billy K. Poon <bkpoon@lbl.gov>
2024-11-02 22:26:15 -0700

    Build coot, add automake and glib dependencies

--- recipes/coot/build.sh
+++ recipes/coot/build.sh
@@ -2,6 +2,10 @@
 
 CORES=4
 
+echo "Building dependencies"
+echo "====================="
+echo
+
 # fftw
 echo "Building fftw"
 cd fftw
@@ -51,6 +55,42 @@ cd clipper
 	--enable-ccp4    \
 	--enable-minimol \
 	--enable-cns
+make -j ${CORES}
+make install
+cd ..
+echo
+
+# coot
+echo "Building coot"
+echo "============="
+echo
+
+cd coot
+
+# automake (from build-it-3-3)
+rm -rf autom4te.cache
+if [ -e ltmain.sh    ] ; then rm ltmain.sh    ; fi
+if [ -e config.guess ] ; then rm config.guess ; fi
+if [ -e config.sub   ] ; then rm config.sub   ; fi
+./autogen.sh
+
+export PKG_CONFIG_PATH=${PREFIX}/lib/pkgconfig
+
+./configure \
+  --prefix=${PREFIX} \
+  --with-fftw-prefix=${PREFIX} \
+  SHELL=/bin/bash \
+  PYTHON=python3 \
+  COOT_BUILD_INFO_STRING="$coot_build_info_string" \
+  --with-boost=${PREFIX}  \
+  --with-boost-libdir=${PREFIX}/lib  \
+  --with-enhanced-ligand-tools \
+  --with-rdkit-prefix=${PREFIX} \
+  --with-guile \
+  --disable-static \
+  --with-glib-prefix=${PREFIX} \
+  --with-gtk-prefix==${PREFIX} \
+  --with-sysroot=${PREFIX}
 make -j ${CORES}
 make install
 cd ..
