cc7248839d8b90881d11a525cf6bfbd08a3046d7
Billy K. Poon <bkpoon@lbl.gov>
2025-01-28 06:36:59 -0800

    debug

--- recipes/libcifpp/build.bat
+++ recipes/libcifpp/build.bat
@@ -3,4 +3,4 @@ cmake %CMAKE_ARGS% --trace-expand -S . -B build -DCMAKE_INSTALL_PREFIX=%PREFIX% 
 cmake --build build
 cmake --install build
 
-ctest --test-dir build
+ctest --output-on-failure  --test-dir build
