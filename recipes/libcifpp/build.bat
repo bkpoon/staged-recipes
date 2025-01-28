ea86b40a66bd8f917ee701c4135ab56279febb4d
Billy K. Poon <bkpoon@lbl.gov>
2025-01-28 06:12:32 -0800

    Build shared libraries

--- recipes/libcifpp/build.bat
+++ recipes/libcifpp/build.bat
@@ -1,4 +1,4 @@
-cmake %CMAKE_ARGS% --trace-expand -S . -B build -DCMAKE_INSTALL_PREFIX=%PREFIX%
+cmake %CMAKE_ARGS% --trace-expand -S . -B build -DCMAKE_INSTALL_PREFIX=%PREFIX% -DBUILD_SHARED_LIBS=ON
 
 cmake --build build
 cmake --install build
--- recipes/libcifpp/build.sh
+++ recipes/libcifpp/build.sh
@@ -2,7 +2,10 @@
 set -xe
 
 # instructions from https://github.com/PDB-REDO/libcifpp#building
-cmake ${CMAKE_ARGS} --trace-expand -S . -B build -DCMAKE_INSTALL_PREFIX=${PREFIX} -DCMAKE_CXX_COMPILER=${CXX}
+cmake ${CMAKE_ARGS} --trace-expand -S . -B build \
+  -DCMAKE_INSTALL_PREFIX=${PREFIX} \
+  -DBUILD_SHARED_LIBS=ON \
+  -DCMAKE_CXX_COMPILER=${CXX}
 
 cmake --build build
 cmake --install build
